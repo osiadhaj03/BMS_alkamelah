ab424f86cbff81bf26b30f7d5a3bfd44fd0411e1
osaidhaj <osaidhaj03@gmail.com>
2026-01-04 19:31:17 +0300

    feat: Implement custom login page with background image and styled form.

---
+++ resources/views/vendor/filament-panels/pages/auth/login.blade.php
@@ -0,0 +1,32 @@
+<x-filament-panels::page.simple>
+    {{-- Custom Background Wrapper --}}
+    <div class="fixed inset-0 -z-10">
+        <img 
+            src="{{ asset('images/login-bg.jpg') }}" 
+            alt="Login Background" 
+            class="h-full w-full object-cover"
+        />
+        <div class="absolute inset-0 bg-black/50 backdrop-blur-sm"></div>
+    </div>
+
+    @if (filament()->hasRegistration())
+        <x-slot name="subheading">
+            {{ __('filament-panels::pages/auth/login.actions.register.before') }}
+
+            {{ $this->registerAction }}
+        </x-slot>
+    @endif
+
+    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_BEFORE, scopes: $this->getRenderHookScopes()) }}
+
+    <x-filament-panels::form wire:submit="authenticate">
+        {{ $this->form }}
+
+        <x-filament-panels::form.actions
+            :actions="$this->getCachedFormActions()"
+            :full-width="$this->hasFullWidthFormActions()"
+        />
+    </x-filament-panels::form>
+
+    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_AFTER, scopes: $this->getRenderHookScopes()) }}
+</x-filament-panels::page.simple>
