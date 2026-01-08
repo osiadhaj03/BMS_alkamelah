982ed624d4df07863dad8797cf97cd228e3ba7f8
osiadhaj03 <osiadhaj03@users.noreply.github.com>
2026-01-09 00:24:18 +0300

    refactor: add notifications and broadcasting to admin panel configuration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->notifications()
             ->colors([
                 'primary' => Color::Green,
                 'secondary' => Color::Green,
@@ -39,6 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->brandName('نظام إدارة المكتبة')
             ->darkMode(true)
+            ->broadcasting()
             ->maxContentWidth('full')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
