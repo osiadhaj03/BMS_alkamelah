cab1f51adf38ac973928f8edc5c2314a12a4bd1e
osiadhaj03 <osiadhaj03@users.noreply.github.com>
2026-01-23 01:46:32 +0300

    09

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+
     <title>@yield('title', 'المكتبة الكاملة') | BMS</title>
 
     <!-- Fonts -->
