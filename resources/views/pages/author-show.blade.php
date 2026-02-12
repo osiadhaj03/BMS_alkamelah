9ed12f413091338e3382921c2724288e51ed6640
osiadhaj03 <osiadhaj03@users.noreply.github.com>
2026-02-13 01:16:07 +0300

    fix: Update death date display format in author show page

--- resources/views/pages/author-show.blade.php
+++ resources/views/pages/author-show.blade.php
@@ -61,7 +61,7 @@
                                             d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z">
                                         </path>
                                     </svg>
-                                    توفي: {{ $author->death_date->format('Y') }} هـ
+                                    توفي: {{ $author->death_date }} هـ
                                 </span>
                             @elseif($author->is_living)
                                 <span
